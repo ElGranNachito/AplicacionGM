ca23cbb02ec9dbeeff7ab1725c45e70a78adc971
Crazy474747 <58375872+Crazy474747@users.noreply.github.com>
2020-11-28 21:52:38 -0300

    Un poco de embellecimiento

---
+++ AppGM/AppGM/Paginas/Mensajes/CreacionDeRol/Creacion De Personajes/UserControlCreacionRol_CreacionHabilidad.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AppGM
+{
+    /// <summary>
+    /// Lógica de interacción para UserControlCreacionRol_CreacionMagia.xaml
+    /// </summary>
+    public partial class UserControlCreacionRol_CreacionHabilidad : UserControl
+    {
+        public UserControlCreacionRol_CreacionHabilidad()
+        {
+            InitializeComponent();
+        }
+    }
+}
