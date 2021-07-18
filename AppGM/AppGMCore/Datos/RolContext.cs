c620c2855d0060184d42d0b3bdf9a37bd4e2ea1a
Contykpo <samu.1furlanich@gmail.com>
2021-07-18 00:54:56 -0300

    Descripcion
    -Mapeado de ModeloFuncion y relaciones con ModeloVariableFuncion.

--- AppGM/AppGMCore/Datos/RolContext.cs
+++ AppGM/AppGMCore/Datos/RolContext.cs
@@ -775,6 +775,24 @@ namespace AppGM.Core
                 .HasOne(ti => ti.Rol)
                 .WithOne(r => r.AmbienteGlobal)
                 .HasForeignKey<TIRolAmbiente>(ti => ti.IdRol);
+
+            // Funcion:
+
+            modelBuilder.Entity<ModeloFuncion>().ToTable("ModeloFuncion").HasNoDiscriminator();
+
+            // - Funcion varibale
+            modelBuilder.Entity<TIFuncionVariable>().HasKey(e => new { e.IDFuncion, e.IDVariable});
+
+            modelBuilder.Entity<TIFuncionVariable>()
+                .HasOne(ti => ti.Variable);
+
+            modelBuilder.Entity<TIFuncionVariable>()
+                .HasOne(ti => ti.Funcion)
+                .WithMany(r => r.VariablesPersistentes)
+                .HasForeignKey(ti => ti.IDFuncion);
+
+            //TODO: Agregar relaciones de ModeloFuncion con ModeloEfecto y ModeloHabilidad en un futuro si se las necesita. 
+
         }
         #endregion
     }
