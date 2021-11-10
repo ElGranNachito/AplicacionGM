6256f09543977a2393353d10134a1a65422c528f
Crazy474747 <58375872+Crazy474747@users.noreply.github.com>
2021-11-10 13:59:45 -0300

    Revert "espero que no se rompa 32"
    This reverts commit b110a9c373d3a194f879ee4061dd2b3574dd30d9.

--- AppGM/AppGMCore/Helpers/EnumHelpers.cs
+++ AppGM/AppGMCore/Helpers/EnumHelpers.cs
@@ -34,14 +34,8 @@ namespace AppGM.Core
         public static List<EComportamientoAcumulativo> ComportamientosAcumulativosDisponibles => Enum.GetValues(typeof(EComportamientoAcumulativo)).Cast<EComportamientoAcumulativo>().ToList();
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
         public static List<ETipoItem> TiposItemDisponibles => Enum.GetValues(typeof(ETipoItem)).Cast<ETipoItem>().ToList();
         public static List<EEstadoPortacion> EstadosDePortacionDisponibles => Enum.GetValues(typeof(EEstadoPortacion)).Cast<EEstadoPortacion>().ToList();
-=======
-        public static List<EEstadoPortacion> EstadosDePortacionDisponibles => Enum.GetValues(typeof(EEstadoPortacion)).Cast<EEstadoPortacion>().ToList();
-
-        public static List<ETipoItem> TiposItemDisponibles => Enum.GetValues(typeof(ETipoItem)).Cast<ETipoItem>().ToList();
->>>>>>> parent of 5d77f96 (Ver descripcion)
 =======
         public static List<EEstadoPortacion> EstadosDePortacionDisponibles => Enum.GetValues(typeof(EEstadoPortacion)).Cast<EEstadoPortacion>().ToList();
 
@@ -444,9 +438,6 @@ namespace AppGM.Core
 
 	        return sBuilder.AppendJoin(", ", flagsActivas).ToString();
 <<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 5d77f96 (Ver descripcion)
-=======
 >>>>>>> parent of 5d77f96 (Ver descripcion)
 =======
 >>>>>>> parent of 5d77f96 (Ver descripcion)
